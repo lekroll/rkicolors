24910f41a2efed8f71033dc5aa90e53291331c91
Lars Eric Kroll <32699019+lekroll@users.noreply.github.com>
2018-08-29 14:46:28 +0200

    Add files via upload

--- inst/doc/example_plot.R
+++ inst/doc/example_plot.R
@@ -1,7 +1,10 @@
 library("rkicolors")
 library("ggplot2")
 library("extrafont")
-ggplot(iris, aes(Sepal.Width, Sepal.Length, color = Species)) +   geom_point() +
-theme_rki(base_family="ScalaSansLF-Regular", bold_family="ScalaSansLF-Bold", base_size = 10) +
+ggplot(iris, aes(Sepal.Width, Sepal.Length, color = Species)) +
+   geom_point() +
+theme_rki(base_family="ScalaSansLF-Regular",
+          bold_family="ScalaSansLF-Bold",
+          base_size = 8) +
 scale_color_rki()
-ggsave("inst/doc/example_plot.png", dpi=200, width = 4, height=3)
+ggsave("inst/doc/example_plot.png", dpi=300, width = 4, height=2.25)
